b37092cf53eec972ae0761ef1730269fa569cb39
Kartik Singh Kushwah <kartiksinghk12@gmail.com>
2023-05-16 20:18:01 +0530

    add first section component

---
+++ src/components/section1.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../css/section1.css";
+
+export default function MyComponent() {
+
+	return (
+		<>
+			<section className="about" id="about">
+				<span className="about__info">
+					<b className="about__text">
+						<span>In</span>
+						<span>Chronicle</span>
+						<span>everything</span>
+						<span>is</span>
+						<span>made</span>
+						<span>with</span>
+						<span>Blocks</span>
+						<span>that</span>
+						<span>come</span>
+						<span>with</span>
+						<span>pixel</span>
+						<span>perfect</span>
+						<span>design,</span>
+						<span>interactivity</span>
+						<span>and</span>
+						<span>motion</span>
+						<span>out</span>
+						<span>of</span>
+						<span>the</span>
+						<span>box.</span>
+						<span>Instead</span>
+						<span>of</span>
+						<span>designing</span>
+						<span>from</span>
+						<span>scratch,</span>
+						<span>simply</span>
+						<span>choose</span>
+						<span>the</span>
+						<span>right</span>
+						<span>one</span>
+						<span>from</span>
+						<span>our</span>
+						<span>library</span>
+						<span>of</span>
+						<span>blocks</span>
+						<span>and</span>
+						<span>see</span>
+						<span>the</span>
+						<span>magic</span>
+						<span>unfold.</span>
+					</b>
+				</span>
+			</section>
+		</>
+	);
+}
